d6ebe5a4f61cb406e8b3e0920c388dad557e1b2c
Adrian Gushin <adrian.gushin@emory.edu>
2023-11-04 12:38:42 -0400

    update

--- process_emotion_corpus.py
+++ process_emotion_corpus.py
@@ -1,12 +1,43 @@
 import pandas as pd
 import os.path
+import spacy
+import numpy as np
+
+def dict_add(dictionary, key, value):
+    if dictionary.get(key) is None:
+        dictionary[key] = value
+    else:
+        dictionary[key] += value
 
 def main():
-    if not os.path.isfile("emotion-corpus/corpus.csv"):
+    if not os.path.isfile("emotion-corpus/emotion-phrases.csv"):
         df1 = pd.read_csv("emotion-corpus/test.csv")
         df2 = pd.read_csv("emotion-corpus/training.csv")
         df3 = pd.read_csv("emotion-corpus/validation.csv")
         pd.concat([df1, df2, df3]).to_csv("emotion-corpus/emotion-phrases.csv", index=False)
+
+    wordmap = dict()
+
+    dataloader = pd.read_csv("emotion-corpus/emotion-phrases.csv")
+    nlp = spacy.load("en_core_web_md")
+    for index, data in dataloader.iterrows():
+        phrase = data['text']
+        label = data['label']
+        jitter = np.random.uniform(.95, 1)
+        score = 0
+        if label == 0 or label == 3:
+            score = -1 * jitter
+        elif label == 1 or label == 2:
+            score = 1 * jitter
+        else:
+            continue
+        
+        tokens = nlp(phrase)
+        for token in tokens:
+            if not token.is_stop:
+                dict_add(wordmap, token, score)
+    print(wordmap)
+                
         
 if __name__ == "__main__":
     main()
