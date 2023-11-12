f4763f5cd23cfbc1cedcb156864369b236986ee8
Adrian Gushin <adrian.gushin@emory.edu>
2023-11-11 23:47:50 -0500

    added weighting tensor calc

--- model.py
+++ model.py
@@ -1,8 +1,9 @@
 import pandas as pd
+import sys
 import torch
 from torch.utils.data import Dataset
-import sys
 from torch.utils.data import DataLoader
+from torch import nn
 
 class TextData(Dataset):
     def __init__(self, x, y, mapping):
@@ -28,6 +29,31 @@ class TextData(Dataset):
         while len(encodings) < self.max_len:
             encodings.append(self.padding_indx)
         return torch.LongTensor(encodings), label
+    
+class SentimentModel(nn.Module):
+    def __init__(self, vocab_size, embedding_dims, padding_index, emote_map):
+        super().__init__()
+        self.emote_map = emote_map
+        self.embeddings = nn.Embedding(vocab_size, embedding_dims, padding_idx=padding_index)
+
+    def get_emote_weights(self, input):
+        weights = torch.ones(len(input))
+        for index, id in enumerate(input):
+            id = id.item()
+            if id in self.emote_map:
+                weights[index] = self.emote_map[id]
+        return weights
+
+    def forward(self, input):
+        return None
+
+def test_val(emote_map, input):
+    weights = torch.ones(len(input))
+    for index, id in enumerate(input):
+        id = id.item()
+        if emote_map.get(id) is not None:
+            weights[index] = emote_map[id]
+    return weights
 
 def load_emotion():
     path = "data/emotion/"
@@ -51,6 +77,14 @@ def build_vocab(data):
 def get_word_mapping(vocab):
     return {word: i for i, word in enumerate(vocab)}
 
+def create_emotions_map(corpus, vocab_map):
+    mapping = dict()
+    for _, row in corpus.iterrows():
+        token = row['token']
+        if vocab_map.get(token) is not None:
+            mapping[vocab_map.get(token)] = row['score']
+    return mapping
+
 def main():
     dataset = sys.argv[1] if sys.argv[1] is not None else "emotion"
     if dataset == "emotion":
@@ -71,6 +105,8 @@ def main():
 
     trainloader = DataLoader(traindata, batch_size=32)
     testloader = DataLoader(testdata, batch_size=32)
+    emotions_map = create_emotions_map(emotion_corpus, vocab_map)
+    print(emotions_map)
 
 if __name__ == "__main__":
     main()
